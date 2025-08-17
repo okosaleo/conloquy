660fee8c8cf5135c3f125832f496af2346dbb075
okosaleo <okosaleonel@gmail.com>
2025-08-17 01:49:45 +0100

    finsihed for the day

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,14 +2,16 @@
 import { LoadingState } from '@/components/loading-state';
 import { useTRPC } from '@/trpc/client';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { DataTable } from '../components/data-table';
+import { columns } from '../components/columns';
 
 export const AgentView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div className='w-full'>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className='flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4'>
+      <DataTable data={data} columns={columns} />
     </div>
   );
 };
