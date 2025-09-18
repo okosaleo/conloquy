4890c7e222dcdbe96731f1ac896b4a8f2bb13900
okosaleo <okosaleonel@gmail.com>
2025-09-18 19:52:49 +0100

    final changes before testing

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,15 +10,7 @@ export default function Layout({
          <div className=" flex lg:flex-row flex-col gap-4">
               <div className="relative lg:w-1/2 w-full lg:h-[110vh] h-[40vh]">
               <Image src="/signImage.jpg" alt="Sign In Image" fill className="object-cover  " />
-            <div className="z-10 absolute top-4 left-1/2 transform -translate-x-1/2 md:block hidden">
-            <Image 
-              src="/C.png" 
-              alt="Conloquy" 
-              width={100} 
-              height={100} 
-              className="object-cover bg-white rounded-full" 
-            />
-          </div>
+            
               </div>
               {children}
               
