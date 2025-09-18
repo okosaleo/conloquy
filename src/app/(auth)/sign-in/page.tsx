39526d662891bbe8bf01f93576a0cd34cde43a60
okosaleo <okosaleonel@gmail.com>
2025-09-18 21:04:03 +0100

    correcting mistakes

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,17 +1,9 @@
-import { auth } from "@/lib/auth";
+
 import SignInView from "@/modules/auth/sign-in-view";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 
 export default async function SignInPage() {
-   const session = await auth.api.getSession({
-      headers: await headers(),
-    });
-  
-    if (!!session) {
-      redirect("/");
-    }
+   
   return (
    <SignInView />
   )
